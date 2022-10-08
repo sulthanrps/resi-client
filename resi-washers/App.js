9685c980abb7eb785e2cc438c482738c784bcaeb
Arifinwidy02 <arifinwidy@gmail.com>
2022-10-08 16:50:04 +0700

    fitur-login-washer-done

--- resi-washers/App.js
+++ resi-washers/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Register } from "./screens/Register";
+import { Login_washer } from "./screens/Login_washer";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Register></Register>
+      <Login_washer></Login_washer>
+      {/* <Register></Register> */}
     </View>
   );
 }
