426ea716327df4a1bbdb701a228089d3f0018502
sulthanrps <suthanrafips@gmail.com>
2022-10-10 22:43:19 +0800

    configuration for looking for washer page

--- resi-customer/App.js
+++ resi-customer/App.js
@@ -12,30 +12,6 @@ import CreateBook from './screens/Book/CreateBook';
 import LookingWasher from './screens/Book/BookFlow/LookingWasher';
 import BookTaken from './screens/Book/BookFlow/BookTaken';
 import WasherTracker from './screens/Book/BookFlow/WasherTracker';
-// e.g in your index.js
-import {
-  // en,
-  // fr,
-  // nl,
-  // de,
-  // pl,
-  // pt,
-  // ar,
-  // ko
-  // fr
-  enGB,
-  registerTranslation,
-} from 'react-native-paper-dates'
-// registerTranslation('en', en)
-// registerTranslation('fr', fr)
-// registerTranslation('nl', nl)
-// registerTranslation('pl', pl)
-// registerTranslation('pt', pt)
-// registerTranslation('de', de)
-// registerTranslation('ar', ar)
-// registerTranslation('ko', ko)
-// registerTranslation('fr', fr)
-registerTranslation('en-GB', enGB)
 
 const Stack = createNativeStackNavigator();
 
@@ -64,7 +40,9 @@ export default function App() {
         <Stack.Screen options={{
           headerShown: false
         }} name='CreateBook' component={CreateBook}></Stack.Screen>
-        <Stack.Screen name='LookingWasher' component={LookingWasher}></Stack.Screen>
+        <Stack.Screen options={{
+          headerShown: false
+        }} name='LookingWasher' component={LookingWasher}></Stack.Screen>
         <Stack.Screen name='BookTaken' component={BookTaken}></Stack.Screen>
         <Stack.Screen name='WasherTracker' component={WasherTracker}></Stack.Screen>
       </Stack.Navigator>
